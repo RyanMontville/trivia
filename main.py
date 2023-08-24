4372e818712f8535d15d0502ab3677c1f13b727d
Ryan Montville <ryan.a.montville@gmail.com>
2023-08-24 16:44:06 -0400

    created the pages for the app, laying out basic structure of each page

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template, redirect, url_for, request
+import requests
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
