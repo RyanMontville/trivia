c1a7922653753afda85b2af8652fa73ca70604bc
Ryan Montville <ryan.a.montville@gmail.com>
2023-08-25 22:43:29 -0400

    started method to check answer and go to next question

--- main.py
+++ main.py
@@ -70,7 +70,16 @@ def game():
 
 @app.route("/answer/<choice>", methods=["GET"])
 def check(choice):
-    return render_template("answer.html", user_answer=choice)
+    correct_answer = trivia.current_question.correct_answer
+    number_of_questions = len(trivia.question_list)
+    message = ""
+    has_next = True
+    if trivia.question_number == number_of_questions:
+        has_next = False
+    if correct_answer["url"] == choice:
+        message = "Correct"
+    message = f"Incorrect. The correct answer was {correct_answer['text']}"
+    return render_template("answer.html", message=message, has_next=has_next)
 
 
 if __name__ == "__main__":
--- question_model.py
+++ question_model.py
@@ -5,14 +5,18 @@ class Question:
     def __init__(self, q_text, q_answers, c_answer, q_type):
         self.text = html.unescape(q_text)
         self.answers = html.unescape(q_answers)
-        self.correct_answer = html.unescape(c_answer)
+        correct = html.unescape(c_answer)
+        self.correct_answer = {
+            "text": html.unescape(c_answer),
+            "url": html.unescape(c_answer).replace(" ", "-").lower()
+        }
         self.question_type = q_type
         self.urls = self.answer_urls()
 
     def answer_urls(self):
         urls = []
         for answer in self.answers:
-            removed = answer.replace(" ", "-")
+            removed = answer.replace(" ", "-").lower()
             new_dict = {"text": answer, "url": removed}
             urls.append(new_dict)
         return urls
